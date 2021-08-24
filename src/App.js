9ff7af6674587b73f882cef791a95a77c754f919
dmquinn <danielmurphyberling@gmail.com>
2021-08-24 20:01:39 +0200

    updated oscillator options

--- src/App.js
+++ src/App.js
@@ -1,151 +1,143 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 import * as Tone from "tone";
 import classNames from "classnames";
 
-// Function which creates a 5x8 grid,
-// with our chosen notes on the vertical axis
 function GenerateGrid() {
-	const grid = [];
-	for (let i = 0; i < 8; i++) {
-		let column = [
-			{ note: "C", isActive: false },
-			{ note: "D#", isActive: false },
-			{ note: "G", isActive: false },
-			{ note: "A", isActive: false },
-			{ note: "C#", isActive: false },
-		];
-		grid.push(column);
-	}
-	return grid;
+  const grid = [];
+  for (let i = 0; i < 8; i++) {
+    let column = [
+      { note: "C", isActive: false },
+      { note: "D#", isActive: false },
+      { note: "G", isActive: false },
+      { note: "A", isActive: false },
+      { note: "C#", isActive: false },
+    ];
+    grid.push(column);
+  }
+  return grid;
 }
 
-// Our chosen octave for our five notes. Try changing this for higher or lower notes
 const CHOSEN_OCTAVE = "3";
 
 export default function App() {
-	
-	const [grid, setGrid] = useState(GenerateGrid());
+  const [grid, setGrid] = useState(GenerateGrid());
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentColumn, setCurrentColumn] = useState(null);
+  const [synthType, setSynthType] = useState("sine");
 
-	const [isPlaying, setIsPlaying] = useState(false);
+  const synth = new Tone.PolySynth().toDestination();
+  useEffect(() => {
+    synth.set({ oscillator: { synthType } });
+    console.log(synth.options.oscillator.synthType);
+  }, [synthType]);
+  function handleNoteClick(clickedColumn, clickedNote) {
+    let updatedGrid = grid.map((column, columnIndex) =>
+      column.map((cell, cellIndex) => {
+        let cellCopy = cell;
 
-	const [currentColumn, setCurrentColumn] = useState(null);
+        if (columnIndex === clickedColumn && cellIndex === clickedNote) {
+          cellCopy.isActive = !cell.isActive;
+        }
 
-	//Notice the new PolySynth in use here, to support multiple notes at once
-	const synth = new Tone.PolySynth().toDestination();
+        return cellCopy;
+      })
+    );
 
-	// Updates our Grid's state
-	// Written to be intelligble, not performant
-	function handleNoteClick(clickedColumn, clickedNote) {
-		// Shallow copy of our grid with updated isActive
-		let updatedGrid = grid.map((column, columnIndex) =>
-			column.map((cell, cellIndex) => {
-				let cellCopy = cell;
+    setGrid(updatedGrid);
+  }
 
-				// Flip isActive for the clicked note-cell in our grid
-				if (
-					columnIndex === clickedColumn &&
-					cellIndex === clickedNote
-				) {
-					cellCopy.isActive = !cell.isActive;
-				}
+  const PlayMusic = async () => {
+    let music = [];
 
-				return cellCopy;
-			})
-		);
+    grid.map((column) => {
+      let columnNotes = [];
+      column.map(
+        (shouldPlay) =>
+          shouldPlay.isActive &&
+          columnNotes.push(shouldPlay.note + CHOSEN_OCTAVE)
+      );
+      music.push(columnNotes);
+    });
 
-		//Updates the grid with the new note toggled
-		setGrid(updatedGrid);
-	}
+    await Tone.start();
 
-	const PlayMusic = async () => {
-		// Variable for storing our note in a appropriate format for our synth
-		let music = [];
+    const Sequencer = new Tone.Sequence(
+      (time, column) => {
+        setCurrentColumn(column);
 
-		grid.map((column) => {
-			let columnNotes = [];
-			column.map(
-				(shouldPlay) =>
-					//If isActive, push the given note, with our chosen octave
-					shouldPlay.isActive &&
-					columnNotes.push(shouldPlay.note + CHOSEN_OCTAVE)
-			);
-			music.push(columnNotes);
-		});
+        synth.triggerAttackRelease(music[column], "8n", time);
+      },
+      [0, 1, 2, 3, 4, 5, 6, 7],
+      "8n"
+    );
 
-		// Starts our Tone context
-		await Tone.start();
+    if (isPlaying) {
+      // Turn of our player if music is currently playing
+      setIsPlaying(false);
+      setCurrentColumn(null);
 
-		// Tone.Sequence()
-		//@param callback
-		//@param "events" to send with callback
-		//@param subdivision  to engage callback
-		const Sequencer = new Tone.Sequence(
-			(time, column) => {
-				// Highlight column with styling
-				setCurrentColumn(column);
+      await Tone.Transport.stop();
+      await Sequencer.stop();
+      await Sequencer.clear();
+      await Sequencer.dispose();
 
-				//Sends the active note to our PolySynth
-				synth.triggerAttackRelease(music[column], "8n", time);
-			},
-			[0, 1, 2, 3, 4, 5, 6, 7],
-			"8n"
-		);
+      return;
+    }
+    setIsPlaying(true);
+    // Toggles playback of our musical masterpiece
+    await Sequencer.start();
+    await Tone.Transport.start();
+  };
+  const handleChangeType = (e) => {
+    console.log("type", e.target.value);
+    setSynthType(e.target.value);
+  };
 
-		if (isPlaying) {
-			// Turn of our player if music is currently playing
-			setIsPlaying(false);
-			setCurrentColumn(null);
-
-			await Tone.Transport.stop();
-			await Sequencer.stop();
-			await Sequencer.clear();
-			await Sequencer.dispose();
-
-			return;
-		}
-		setIsPlaying(true);
-		// Toggles playback of our musical masterpiece
-		await Sequencer.start();
-		await Tone.Transport.start();
-	};
-
-	return (
-		<div className="App">
-			<div className="note-wrapper">
-				{grid.map((column, columnIndex) => (
-					<div
-						className={classNames("note-column", {
-							"note-column--active":
-								currentColumn === columnIndex,
-						})}
-						key={columnIndex + "column"}
-					>
-						{column.map(({ note, isActive }, noteIndex) => (
-							<NoteButton
-								note={note}
-								isActive={isActive}
-								onClick={() =>
-									handleNoteClick(columnIndex, noteIndex)
-								}
-								key={note + columnIndex}
-							/>
-						))}
-					</div>
-				))}
-			</div>
-			<button className="play-button" onClick={() => PlayMusic()}>
-				{isPlaying ? (
-					<i class="fas fa-pause"></i>
-				) : (
-					<i class="fas fa-play"></i>
-				)}
-			</button>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="note-wrapper">
+        {grid.map((column, columnIndex) => (
+          <div
+            className={classNames("note-column", {
+              "note-column--active": currentColumn === columnIndex,
+            })}
+            key={columnIndex + "column"}
+          >
+            {column.map(({ note, isActive }, noteIndex) => (
+              <NoteButton
+                note={note}
+                isActive={isActive}
+                onClick={() => handleNoteClick(columnIndex, noteIndex)}
+                key={note + columnIndex}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+      <button className="play-button" onClick={() => PlayMusic()}>
+        {isPlaying ? <i class="fas fa-pause"></i> : <i class="fas fa-play"></i>}
+      </button>
+      <div className="types">
+        <button value="sine" onClick={handleChangeType}>
+          TYPE
+        </button>{" "}
+        <button value="square" onClick={handleChangeType}>
+          TYPE
+        </button>{" "}
+        <button value="sawtooth" onClick={handleChangeType}>
+          TYPE
+        </button>{" "}
+      </div>
+    </div>
+  );
 }
 
 const NoteButton = ({ note, isActive, ...rest }) => {
-	const classes = isActive ? "note note--active" : "note";
-	return <button className={classes} {...rest}></button>;
+  const classes = isActive ? "note note--active" : "note";
+  return (
+    <>
+      <button className={classes} {...rest}></button>
+    </>
+  );
 };
